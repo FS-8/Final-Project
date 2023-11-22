d8ddbc2405fcb6b792882f101fd6082472a441b0
adtya27 <adityanfirdaus@gmail.com>
2023-11-22 19:24:27 +0700

    styling tipis

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,19 +4,19 @@ import { AiOutlineUnlock } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 function Login() {
   return (
-    <div>
+    <div className="mx-auto">
       <div className="bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative transition-all duration-200">
         <h1 className="text-5xl text-white font-bold text-center mb-6">Login</h1>
         <form action="">
           <div className="relative my-4">
             <input
               type="email"
-              className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              className="block w-72 py-2.5 px-0 text-lg text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
               placeholder=""
             />
             <label
               htmlFor=""
-              className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="absolute text-xl text-white duration-300 transform-translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Your Email
             </label>
@@ -25,12 +25,12 @@ function Login() {
           <div className="relative my-4">
             <input
               type="password"
-              className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              className="block w-72 py-3.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
               placeholder=""
             />
             <label
               htmlFor=""
-              className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="absolute text-xl text-white duration-300 transform-translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Your Password
             </label>
@@ -41,7 +41,7 @@ function Login() {
               <input type="checkbox" name="" id="" />
               <label htmlFor="Remember Me">Remember Me</label>
             </div>
-            <Link to="" className="text-blue-500">
+            <Link to="" className="text-fuchsia-800">
               Forgot Password?
             </Link>
           </div>
@@ -51,7 +51,7 @@ function Login() {
           <div>
             <span className="m-4">
               New Here?{' '}
-              <Link className="text-blue-500" to="/Register">
+              <Link className="text-fuchsia-800" to="/Register">
                 Create an Account
               </Link>
             </span>
